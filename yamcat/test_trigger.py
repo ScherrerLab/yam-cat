14a08f3bb480aa081e71c77ea276b17146506aa4
kushalkolar <kushalkolar@gmail.com>
2021-09-15 18:04:37 -0400

    hardware trigger test

---
+++ yamcat/test_trigger.py
@@ -0,0 +1,20 @@
+from pyfirmata import Arduino, util
+from time import sleep
+
+board = Arduino('/dev/ttyACM0')
+board.digital[9].write(0)
+
+framerate = 60
+
+# divide by two for high and low signal outputs
+delay = (1000 / framerate) / 2
+
+HIGH = 1
+LOW = 0
+
+while True:
+    board.digital[9].write(HIGH)
+    sleep(delay)
+    board.digital[9].write(LOW)
+    sleep(delay)
+
